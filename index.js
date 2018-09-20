c9c98b8383502195057486a67fe8620592f5c584
Pawel Karauda <pkarauda@asper.net.pl>
2018-09-20 23:01:56 +0200

    Add GoogleOAuth

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
 const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+
+const keys = require("./config/keys");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send({ hi: "there Arghghgh" });
-});
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback"
+    },
+    (accesToken, refreshToken, profile, done) => {
+      console.log(accesToken, refreshToken, profile, done);
+    }
+  )
+);
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"]
+  })
+);
+
+app.get("/auth/google/callback", passport.authenticate("google"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
