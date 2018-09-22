9e008cd712dd9413a8a601c3879f2e761d1eb6f3
Pawel Karauda <pkarauda@asper.net.pl>
2018-09-22 18:22:41 +0200

    Changed keys to prod and dev

---
+++ config/prod.js
@@ -0,0 +1,6 @@
+module.exports = {
+    googleClientID: process.env.GOOGLE_CLINET_ID,
+    googleClientSecret: process.env.GOOGLE.CLIENT_SECRET,
+    mongoURI: process.env.MONGO_URI,
+    cookieKey: process.env.COOKIE_KEY
+};
